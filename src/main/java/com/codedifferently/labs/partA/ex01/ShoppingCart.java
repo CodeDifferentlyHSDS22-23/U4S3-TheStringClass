eb9f034239147bc24972edf018c2c0523ddc9cd9
Clamjosh <Clamjosh1@users.noreply.github.com>
2024-12-13 14:09:42 -0500

    finished shopping cart

--- src/main/java/com/codedifferently/labs/partA/ex01/ShoppingCart.java
+++ src/main/java/com/codedifferently/labs/partA/ex01/ShoppingCart.java
@@ -1,23 +1,16 @@
 package com.codedifferently.labs.partA.ex01;
 
 public class ShoppingCart {
-    public static String name() {
-        String response = "";
-        String custName = "Hakim Smith";
+    public static void main(String[] args) {
+        String custName = "Steve Smith";
         String firstName;
         int spaceIdx;
-        //int spaceIdx;
-        // Get the index of the space character (" ") in custName.
 
+        // Get the index of the space character (" ") in custName
+        spaceIdx = custName.indexOf(" ");
 
-        // Use the substring method to parse out the first name and print it.
-
-        return response;
-    }
-    public static void main(String[] args) {
-        String nameOut = name();
-        System.out.println(nameOut);
-
-
+        // Use the substring method to parse out the first name and print it
+        firstName = custName.substring(0, spaceIdx);
+        System.out.println("First Name: " + firstName);
     }
 }
